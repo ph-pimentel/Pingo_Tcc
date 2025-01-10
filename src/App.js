45d2c3420fbec483ab6a1542ac0bee7674cd2a7a
Pedro <pedropimentel17henrique@gmail.com>
2025-01-10 20:41:01 -0300

    Primeiras funcionalidades do carrossel e botoes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import Title from "./components/Title"
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Carrossel from "./components/Carrossel";
 
 function App() {
     return (
      <>   
        <Header/>
        <Container>
+        <Title></Title>
+        <Carrossel></Carrossel>
        </Container>
      </>
     );
